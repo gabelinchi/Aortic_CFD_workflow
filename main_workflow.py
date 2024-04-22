6b252ad07902166ac61a9314e8ac48a21af4e82a
lmthomasTU <167747115+lmthomasTU@users.noreply.github.com>
2024-04-22 10:21:09 +0200

    Add files via upload
    Uploaded main_workflow file

---
+++ main_workflow.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 10:16:40 2024
+
+@author: lmorr
+"""
+
+#import modules
+import numpy as np
+
+#use an excessive amount of comments on everything.
