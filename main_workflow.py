16fb9a2de7b560bae1b2ba623725871d6a7a7a86
lmthomas <l.m.thomas@student.tudelft.nl>
2024-04-23 09:59:08 +0200

    Imported modules for mapping

--- main_workflow.py
+++ main_workflow.py
@@ -6,7 +6,12 @@ Created on Mon Apr 22 10:16:40 2024
 """
 
 #import modules
+import sys
+import os.path as osp
+from glob import glob
 import numpy as np
+import pyvista as pv
+import utils as ut
 
 #use an excessive amount of comments on everything.
 #extra line of code changed
