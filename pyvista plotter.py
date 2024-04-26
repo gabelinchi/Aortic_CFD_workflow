d8abae1211a5aa51a1dab8e411ba25a8192d7774
lmthomas <l.m.thomas@student.tudelft.nl>
2024-04-26 09:53:49 +0200

    minor changes

--- pyvista plotter.py
+++ pyvista plotter.py
@@ -9,6 +9,6 @@ aorta_geometry = r'C:\Users\lmorr\Documents\TU\23-24\BEP\case_0163\meshes\wall.s
 target_profile = r'C:\Users\lmorr\Documents\TU\23-24\BEP\Target_profile\inlet.stl'
 read_file_input = r'C:\Users\lmorr\Documents\TU\23-24\BEP\Velocity_profiles\001_00.vtp'
 read_file_output = r'C:\Users\lmorr\Documents\TU\23-24\BEP\Mapping_output\Mapped_velocity_profile_00.vtp'
-plot_file = pv.read(read_file_input)
+plot_file = pv.read(read_file_output)
 plot_file.plot(show_edges = True)
 
