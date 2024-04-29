0fb00fcea0f14dd7511e1c7e1ff29356956369aa
lmthomas <l.m.thomas@student.tudelft.nl>
2024-04-29 16:46:07 +0200

    edited meshclosing so that it get the edges

--- meshclosing.py
+++ meshclosing.py
@@ -2,6 +2,8 @@ import pyvista as pv
 import numpy as np
 import meshio
 
+adjustment = np.array([0,0,20])
+
 meshio.write('inlet_mmg.vtk', meshio.read('inlet_mmg.mesh'))
 meshio.write('wall_mmg.vtk', meshio.read('wall_mmg.mesh'))
 
@@ -10,3 +12,4 @@ wall = pv.read('wall_mmg.vtk')
 
 inlet_edges = inlet.extract_feature_edges(boundary_edges=True, non_manifold_edges=False, manifold_edges=False, feature_edges=False)
 wall_edges = wall.extract_feature_edges(boundary_edges=True, non_manifold_edges=False, manifold_edges=False, feature_edges=False)
+wall_inlet_edges = wall_edges.clip('-z', origin=(wall.center - adjustment))
