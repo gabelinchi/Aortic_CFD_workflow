1b8a7a2c1955adf26340657b66935ef521dc739d
Yarran <yarranbrunt@gmail.com>
2024-04-30 14:30:10 +0200

    Edited function to work with coords instead of indices

--- reduce_polygons.py
+++ reduce_polygons.py
@@ -6,10 +6,24 @@ def pad(faces):
     num_rows = faces.shape[0]
     return(np.hstack((np.full((num_rows, 1), 3),faces)))
 
-def remove_point_and_fill(polydata, points_to_remove):
+def remove_points_and_fill(polydata, coords_to_remove, plot=False):
+    '''
+    Function that removes a point from a pyvistas polydata object and remeshes the hole left behind
+
+    polydata: pyvista.PolyData object
+    coords_to_remove: numpy array (3 columns) with coords of points to be removed
+    plot: boolean, determines if process stepps are plotted, false by default
+    '''
+    # Convert input to polydata
+    polydata = polydata.extract_surface()
+
     # Split polydata into np arrays
     faces = polydata.faces.reshape(-1, 4)[:,[1,2,3]]
     points = polydata.points
+
+    # Obtain point indices that match input coords
+    matches = (points == coords_to_remove[:, np.newaxis]).all(axis=2)
+    points_to_remove = np.where(matches)[1]
 
     # Loop over points to populate fill_mesh with the pathches
     fill_mesh = pv.PolyData()
@@ -28,20 +42,18 @@ def remove_point_and_fill(polydata, points_to_remove):
 
         # Create a mesh to fill the gap and add to fill_mesh
         fill_mesh = fill_mesh.merge(pv.PolyData(points[edge_points]).delaunay_2d())
-        fill_mesh.plot(show_edges=True)
 
     # Rebuild polydata of mesh to fill (now with the gaps)
     mesh_to_fill = pv.PolyData(points, pad(faces))
-    mesh_to_fill.plot(show_edges=True)
 
     # Combine with the patches
     reduced_surface = fill_mesh.merge(mesh_to_fill).clean()
-    reduced_surface.plot(show_edges=True)
+    if plot==True:
+        fill_mesh.plot(show_edges=True)
+        mesh_to_fill.plot(show_edges=True)
+        reduced_surface.plot(show_edges=True)
     return() # Return pv.PolyData of the reconstructed surface
 
-#------------------------------------------------------------------------------------------------------
-# Load mesh data as pv.Polydata and split into numpy arrays
-inlet_remeshed = pv.read('inlet_mmg.vtk').extract_surface()
-wall_remeshed = pv.read('wall_mmg.vtk').extract_surface()
-
-remove_point_and_fill(wall_remeshed, [1, 2, 102])
+coords =np.array([[5.34289188,17.62569172, -7.65842196],[5.04848285, 19.24025361, -6.50630999],[7.61087718, 26.40826548,  4.68480583],[6.9490417,  26.22630622,  3.47412098],[5.15573387, 18.44954256, -7.09167542],[14.43523888, 10.74629191, -0.39696456],[16.97434086,  9.96784403,  2.8223432 ],[17.49438435,  9.89288418,  3.50644435]])
+print(coords)
+remove_points_and_fill(pv.read('wall_mmg.vtk'), coords, plot=True)
