785b1052154b08e36f46b1ef041e5e876197db01
lmthomas <l.m.thomas@student.tudelft.nl>
2024-05-03 14:02:05 +0200

    Put 3D meshing in a seperate function

--- main_workflow.py
+++ main_workflow.py
@@ -9,6 +9,7 @@ import tetgen as tet
 import remesh
 import meshclosing
 import utils as ut
+import volume_mesh
 
 #paths to the aorta geometry
 
@@ -48,31 +49,10 @@ inlet_reduced = meshclosing.remove_points_and_fill(inlet_remeshed, inlet_point_s
 combined_mesh = (wall_reduced + outlet_reduced+inlet_reduced).clean(tolerance=search_tolerance)
 combined_mesh.plot(show_edges=True)
 
-# create 3D tetmesh from surface mesh
-tetmesh = tet.TetGen(combined_mesh)
-tetmesh.tetrahedralize(**tetgen_parameters)
-grid = tetmesh.grid
-grid.plot(show_edges=True)
+#Make a 3D mesh from the combined mesh
+tetmesh = volume_mesh.volume_meshing(combined_mesh, tetgen_parameters, False)
 
-# Save the generated mesh as a .stl
-grid.save('aorta_tetmesh.vtk', binary=False)
-
-# get cell centroids
-cells = grid.cells.reshape(-1, 5)[:, 1:]
-cell_center = grid.points[cells].mean(1)
-
-# extract cells below the 0 xy plane
-mask = cell_center[:, 2] < 0
-cell_ind = mask.nonzero()[0]
-subgrid = grid.extract_cells(cell_ind)
-
-# advanced plotting
-plotter = pv.Plotter()
-plotter.add_mesh(subgrid, 'lightgrey', lighting=True, show_edges=True)
-plotter.add_mesh(combined_mesh, 'r', 'wireframe')
-plotter.add_legend([[' Input Mesh ', 'r'],
-                    [' Tessellated Mesh ', 'black']])
-plotter.show()
+tetmesh.plot(show_edges = True)
 
 #print(mesh)
 
